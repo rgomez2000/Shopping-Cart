2abb34d14d36780244865a63f0eaccabb3b50156
rgomez2000 <83112451+rgomez2000@users.noreply.github.com>
2024-08-23 16:54:11 -0700

    Added functionality to cart
    Cart now has functionality to calculate the taxes, total and subtotal to the cart.

--- script.js
+++ script.js
@@ -136,6 +136,22 @@ class ShoppingCart {
   getCounts() {
     return this.items.length;
   }
+
+
+
+  calculateTaxes(amount) {
+    return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
+  }
+
+  calculateTotal() {
+    const subTotal = this.items.reduce((total, item) => total + item.price, 0);
+    const tax = this.calculateTaxes(subTotal);
+    this.total = subTotal + tax;
+    cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+    cartTaxes.textContent = `$${tax.toFixed(2)}`;
+    cartTotal.textContent = `$${this.total.toFixed(2)}`;
+    return this.total;
+  }
 };
 
 const cart = new ShoppingCart();
@@ -145,7 +161,8 @@ const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
   (btn) => {
     btn.addEventListener("click", (event) => {
       cart.addItem(Number(event.target.id), products);
-      
+      totalNumberOfItems.textContent = cart.getCounts();
+      cart.calculateTotal();
     })
   }
 );
